953dadc58bf6bee2c1e2b386e819fc9041c8ce55
Vaibhav Thakre <vaibhavthakre@Vaibhavs-MacBook-Air.local>
2026-05-21 11:53:16 +0530

    Add Azure backend for Terraform state

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -9,6 +9,12 @@ terraform {
       version = "~> 3.0"
     }
   }
+  backend "azurerm" {
+    resource_group_name  = "deployment-framework-rg"
+    storage_account_name = "tfstatedeployfw"
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
+  }
 }
 
 provider "azurerm" {
